5dec7aef843b378c87b087eb7398324a904a8f14
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2024-10-30 21:50:20 -0500

    implementacion completa de la terminal inteligente

--- interfaz_algoritmos/lib/main.dart
+++ interfaz_algoritmos/lib/main.dart
@@ -1,17 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:interfaz_algoritmos/algoritmos/problema1/algoritmoFuerzaBruta.dart';
-import 'package:interfaz_algoritmos/algoritmos/problema1/algoritmoProgramacionDinamica.dart';
-import 'package:interfaz_algoritmos/algoritmos/problema1/algoritmoVoraz.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
 
-String cadenaOriginal = 'algorithm';
-String cadenaObjetivo = 'altruistic';
+String cadenaOriginal = '';
+String cadenaObjetivo = '';
 int posicion = 0;
 bool isUpdating = false;
 List resultado = [];
+TextEditingController controllerCadenaOriginal = TextEditingController();
+TextEditingController controllerCadenaObjetivo = TextEditingController();
+TextEditingController controllerCostAdvance = TextEditingController();
+TextEditingController controllerCostDelete = TextEditingController();
+TextEditingController controllerCostReplace = TextEditingController();
+TextEditingController controllerCostInsert = TextEditingController();
+TextEditingController controllerCostKill = TextEditingController();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -41,13 +48,35 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
-  void _changeCadena() async {
+  void _changeCadena(String typeAlgoritmo) async {
+    var url = Uri.http('localhost:4000', '/algoritmo');
+    try {
+      var response = await http.post(url,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: jsonEncode({
+            'algoritmo': typeAlgoritmo,
+            'cadena_original': cadenaOriginal,
+            'cadena_objetivo': cadenaObjetivo,
+            "cost_advance": int.parse(controllerCostAdvance.text),
+            "cost_delete": int.parse(controllerCostDelete.text),
+            "cost_replace":  int.parse(controllerCostReplace.text),
+            "cost_insert": int.parse(controllerCostInsert.text),
+            "cost_kill": int.parse(controllerCostKill.text),
+          }));
+      var respuesta = jsonDecode(response.body);
+      setState(() {
+        resultado = respuesta['resultado'];
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+
     if (resultado.isEmpty) {
       return;
     }
-  print(cadenaOriginal);
-    List<String> operaciones = resultado[1];
+    List<dynamic> operaciones = resultado[1];
 
     for (int i = 0; i < operaciones.length; i++) {
       switch (operaciones[i].contains(' ')
@@ -57,10 +86,8 @@ class _MyHomePageState extends State<MyHomePage> {
           if (!isUpdating) {
             isUpdating = true;
             setState(() {
-              
-            posicion++;
+              posicion++;
             });
-            print(cadenaOriginal);
             await Future.delayed(const Duration(milliseconds: 500));
             isUpdating = false;
           }
@@ -85,9 +112,8 @@ class _MyHomePageState extends State<MyHomePage> {
                   operaciones[i].split(' ')[1] +
                   cadenaOriginal.substring(posicion);
               posicion++;
-              
-            }); 
-             await Future.delayed(const Duration(milliseconds: 500));
+            });
+            await Future.delayed(const Duration(milliseconds: 500));
             isUpdating = false;
           }
           break;
@@ -95,11 +121,11 @@ class _MyHomePageState extends State<MyHomePage> {
           if (!isUpdating) {
             isUpdating = true;
             setState(() {
-              cadenaOriginal = cadenaOriginal.substring(0, posicion) + operaciones[i].split(' ')[3] +
+              cadenaOriginal = cadenaOriginal.substring(0, posicion) +
+                  operaciones[i].split(' ')[3] +
                   cadenaOriginal.substring(posicion + 1);
-                posicion++;
+              posicion++;
             });
-
 
             await Future.delayed(const Duration(milliseconds: 500));
             isUpdating = false;
@@ -109,10 +135,8 @@ class _MyHomePageState extends State<MyHomePage> {
           if (!isUpdating) {
             isUpdating = true;
             setState(() {
-              cadenaOriginal = cadenaOriginal.substring(0, posicion) ;
+              cadenaOriginal = cadenaOriginal.substring(0, posicion);
             });
-
-
 
             await Future.delayed(const Duration(milliseconds: 500));
             isUpdating = false;
@@ -129,116 +153,265 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-       body: Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-          
-            SizedBox(
-             width: 1000,
-             height: 50,
-             
-              child: ListView.builder(                 
-                 itemCount: cadenaOriginal.length,
-                 scrollDirection: Axis.horizontal,
-                 itemBuilder: (context, index) {
-                   return Container(
-                     
-                     width: 40,
-                     height: 40,
-                     decoration: BoxDecoration(
-                       color: index == posicion
-                           ? Colors.green
-                           : Colors.white,
-                       border: Border.all(color: Colors.black),
-                     ),
-                     child: Center(
-                       child: Text(cadenaOriginal[index]),
-                     ),
-                   );
-                 }),
+            Wrap(
+              children: [
+                SizedBox(
+                  height: 50,
+                  width: 200,
+                  child: TextField(
+                    controller: controllerCadenaOriginal,
+
+                    decoration: const InputDecoration(
+                      labelText: 'Cadena Original',
+                      border:  OutlineInputBorder(),
+                      labelStyle: TextStyle(color: Colors.black),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        
+                      cadenaOriginal = value;
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 200,
+                  child: TextField(
+                      controller: controllerCadenaObjetivo,
+
+                      decoration:const InputDecoration(
+                      labelText: 'Cadena Objetivo',
+                      border: OutlineInputBorder(),
+                      labelStyle: TextStyle(color: Colors.black),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        
+                      cadenaObjetivo = value;
+                      });
+                    },
+                  ),
+                ),
+                                const SizedBox(
+                  width: 20,
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 100,
+                  child: TextField(
+                      controller: controllerCostAdvance,
+
+                      decoration:const InputDecoration(
+                      labelText: 'Advance',
+                      border: OutlineInputBorder(),
+                      labelStyle: TextStyle(color: Colors.black),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        controllerCostAdvance.text = value;
+                      });
+                    },
+                  ),
+                ),
+                                const SizedBox(
+                  width: 20,
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 100,
+                  child: TextField(
+                      controller: controllerCostInsert,
+
+                      decoration:const InputDecoration(
+                      labelText: 'Insert',
+                      border: OutlineInputBorder(),
+                      labelStyle: TextStyle(color: Colors.black),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        controllerCostInsert.text = value;
+                      });
+                    },
+                  ),
+                ),
+                                const SizedBox(
+                  width: 20,
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 100,
+                  child: TextField(
+                      controller: controllerCostReplace,
+
+                      decoration:const InputDecoration(
+                      labelText: 'Replace',
+                      border: OutlineInputBorder(),
+                      labelStyle: TextStyle(color: Colors.black),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        controllerCostReplace.text = value;
+                      });
+                    },
+                  ),
+                ),
+                                const SizedBox(
+                  width: 20,
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 100,
+                  child: TextField(
+                      controller: controllerCostDelete,
+
+                      decoration:const InputDecoration(
+                      labelText: 'Delete',
+                      border: OutlineInputBorder(),
+                      labelStyle: TextStyle(color: Colors.black),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        controllerCostDelete.text = value;
+                      });
+                    },
+                  ),
+                ),
+                                const SizedBox(
+                  width: 20,
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 100,
+                  child: TextField(
+                      controller: controllerCostKill,
+
+                      decoration:const InputDecoration(
+                      labelText: 'Kill',
+                      border: OutlineInputBorder(),
+                      labelStyle: TextStyle(color: Colors.black),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        controllerCostKill.text = value;
+                      });
+                    },
+                  ),
+                ),
+
+                
+              ],
             ),
-            if(resultado.isNotEmpty)
-            Text('Costo total: ${resultado[3]}'),
-            
+            const SizedBox(
+              height: 100,
+            ),
+            SizedBox(
+              width: 1200,
+              height: 50,
+              child: ListView.builder(
+                  itemCount: cadenaOriginal.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      width: 40,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: index == posicion ? Colors.green : Colors.white,
+                        border: Border.all(color: Colors.black),
+                      ),
+                      child: Center(
+                        child: Text(cadenaOriginal[index]),
+                      ),
+                    );
+                  }),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            if (resultado.isNotEmpty) Text('Costo total: ${resultado[0]}'),
+            if (resultado.isNotEmpty) Text('Operaciones: ${resultado[2]}'),
           ],
         ),
-        
       ),
-    
-       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: Stack(
         fit: StackFit.expand,
         children: [
-              Positioned(
+          Positioned(
+            bottom: 20,
+            left: 580,
+            height: 70,
+            width: 100,
+            child: FloatingActionButton(
+              heroTag: 'fuerzaBruta',
+              onPressed: () {
+                _changeCadena('fuerza_bruta');
+              },
+              child: const Text('Fuerza Bruta'),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: 400,
+            height: 70,
+            width: 170,
+            child: FloatingActionButton(
+              heroTag: 'programacionDinamica',
+              onPressed: () {
+                _changeCadena('dinamica');
+              },
+              child: const Text('Programación Dinámica'),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: 700,
+            height: 70,
+            width: 70,
+            child: FloatingActionButton(
+              heroTag: 'voraz',
+              onPressed: () {
+                _changeCadena('voraz');
+              },
+              child: const Text('Voraz'),
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            left: 790,
+            height: 70,
+            width: 70,
+            child: FloatingActionButton(
+              heroTag: 'reiniciar',
+              onPressed: () {
+                setState(() {
+                  cadenaOriginal = '';
+                  cadenaObjetivo = '';
+                  posicion = 0;
+                  resultado = [];
+                  controllerCadenaOriginal.clear();
+                  controllerCadenaObjetivo.clear();
+                  controllerCostAdvance.clear();
+                  controllerCostDelete.clear();
+                  controllerCostReplace.clear();
+                  controllerCostInsert.clear();
+                  controllerCostKill.clear();
 
-                bottom: 20,
-                left: 220,
-                height: 70,
-                width: 100,
-                child: FloatingActionButton(
-                  heroTag: 'fuerzaBruta',
-                    onPressed: () {
-                     resultado= algoritmoFuerzaBruta(cadenaOriginal, cadenaObjetivo);
-                    },
-                    child: const Text('Fuerza Bruta'),
-                  ),
-              ),
-                Positioned(
-                  
-                bottom: 20,
-                left: 340,
-                height: 70,
-                width: 170,
-                  child: FloatingActionButton(
-                    heroTag: 'programacionDinamica',
-                    onPressed: () {
-                       resultado = algoritmoProgramacionDinamica(cadenaOriginal, cadenaObjetivo);
-                       _changeCadena();
-                    },
-                    child: const Text('Programación Dinámica'),
-                  ),
-                ),
-                Positioned(
-                  
-
-                bottom: 20,
-                left: 120,
-                height: 70,
-                width: 70,
-                  child: FloatingActionButton(
-                    heroTag: 'voraz',
-                    onPressed: () {
-                     resultado = algoritmoVoraz(cadenaOriginal, cadenaObjetivo);
-                      _changeCadena();
-                    },
-                    child: const Text('Voraz'),
-                  ),
-                ),
-                Positioned(
-                bottom: 20,
-                left: 20,
-                height: 70,
-                width: 70,
-                  child: FloatingActionButton(
-                    heroTag: 'reiniciar',
-                    onPressed: () {
-                      setState(() {
-                        
-                      cadenaOriginal = 'algorithm';
-                      cadenaObjetivo = 'altruistic';
-                      posicion = 0;
-                      
-                      });
-                    },
-                    child: const Text('Reiniciar'),
-                  ),
-                ),
-            
-
+                });
+              },
+              child: const Text('Reiniciar'),
+            ),
+          ),
         ],
-       ),
+      ),
     );
   }
 }
