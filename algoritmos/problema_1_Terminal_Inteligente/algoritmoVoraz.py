5df9b7aa7cf86c152eec619ce6cf2406e0088bb3
JAMM0118 <jhon.alejandro.correounivalle.edu.co>
2024-11-28 16:00:05 -0500

    version final de los algoritmos de la terminal inteligente

--- algoritmos/problema_1_Terminal_Inteligente/algoritmoVoraz.py
+++ algoritmos/problema_1_Terminal_Inteligente/algoritmoVoraz.py
@@ -1,11 +1,5 @@
-COST_ADVANCE = 1
-COST_DELETE = 2
-COST_INSERT = 2
-COST_REPLACE = 3
-COST_KILL = 1
 
-
-def algoritmoVoraz(cadena_original, cadena_objetivo):
+def algoritmoVoraz(cadena_original, cadena_objetivo,cost_advance, cost_delete , cost_insert, cost_replace, cost_kill):
     i = 0
     j = 0
     costo_total = 0
@@ -14,54 +8,54 @@ def algoritmoVoraz(cadena_original, cadena_objetivo):
     cadena_formada = ""
 
     while i < len(cadena_original) and j < len(cadena_objetivo):
-        costo_advance = COST_ADVANCE if cadena_original[i] == cadena_objetivo[j] else float('inf')
-        costo_replace = COST_REPLACE
-        costo_insert = COST_INSERT
-        costo_delete = COST_DELETE
+        costo_advance = cost_advance if cadena_original[i] == cadena_objetivo[j] else float('inf')
+        costo_replace = cost_replace
+        costo_insert = cost_insert
+        costo_delete = cost_delete
 
         if costo_advance <= costo_replace and costo_advance <= costo_insert and costo_advance <= costo_delete:
             operaciones.append(f"advance at position {i}")
             cadena_formada += cadena_original[i]
-            costo_total += COST_ADVANCE
+            costo_total += cost_advance
             conteo_operaciones["advance"] += 1
             i += 1
             j += 1
         elif costo_replace <= costo_advance and costo_replace <= costo_insert and costo_replace <= costo_delete:
             operaciones.append(f"replace {cadena_original[i]} with {cadena_objetivo[j]} at position {i}")
             cadena_formada += cadena_objetivo[j]
-            costo_total += COST_REPLACE
+            costo_total += cost_replace
             conteo_operaciones["replace"] += 1
             i += 1
             j += 1
         elif costo_insert <= costo_advance and costo_insert <= costo_replace and costo_insert <= costo_delete:
             operaciones.append(f"insert {cadena_objetivo[j]} at position {i}")
             cadena_formada += cadena_objetivo[j]
-            costo_total += COST_INSERT
+            costo_total += cost_insert
             conteo_operaciones["insert"] += 1
             j += 1 
         else:
             
             operaciones.append(f"delete {cadena_original[i]} at position {i}")
-            costo_total += COST_DELETE
+            costo_total += cost_delete
             conteo_operaciones["delete"] += 1
             i += 1  
 
     if i < len(cadena_original) and j == len(cadena_objetivo):
-        if COST_KILL < COST_DELETE * (len(cadena_original) - i):
+        if cost_kill < cost_delete * (len(cadena_original) - i):
             operaciones.append(f"kill from position {i}")
-            costo_total += COST_KILL
+            costo_total += cost_kill
             conteo_operaciones["kill"] += 1
         else:
             while i < len(cadena_original):
                 operaciones.append(f"delete {cadena_original[i]} at position {i}")
-                costo_total += COST_DELETE
+                costo_total += cost_delete
                 conteo_operaciones["delete"] += 1
                 i += 1
 
     while j < len(cadena_objetivo):
         operaciones.append(f"insert {cadena_objetivo[j]} at position {i}")
         cadena_formada += cadena_objetivo[j]
-        costo_total += COST_INSERT
+        costo_total += cost_insert
         conteo_operaciones["insert"] += 1
         j += 1
 
@@ -71,7 +65,7 @@ def algoritmoVoraz(cadena_original, cadena_objetivo):
 if __name__ == "__main__":
     cadena_original = input("Ingrese la cadena original: ")
     cadena_objetivo = input("Ingrese la cadena objetivo: ") 
-    costo, ops, conteo_ops, cadena_formada = algoritmoVoraz(cadena_original, cadena_objetivo)
+    costo, ops, conteo_ops, cadena_formada = algoritmoVoraz(cadena_original, cadena_objetivo, 1, 1, 1, 1, 1)
     print("Costo Voraz:", costo)
     print("Operaciones:", ops)
     print("Conteo de operaciones:", conteo_ops)
